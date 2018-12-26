787143abc17c2c34043d8d05c122afd72d421e81
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2018-12-26 23:36:19 +0530

    Add files via upload

---
+++ callstack.js
@@ -0,0 +1,18 @@
+function cto(){
+    console.log("Let's make a gamechangeing product")
+    vpengg()
+}
+function ceo(){
+    console.log("Let's make a dent in the universe")
+    cto()
+}
+
+function vpengg(){
+    console.log("Let's make a new Javascript framework ")
+    techlead()
+}
+function techlead(){
+    console.log('Let\'s port angular to typescript')
+    throw new Error("The code didn't work")
+}
+ceo();
